1bfd30847749115d8f0e123e815cf37c2f01a429
Jardson Sales <jardson.sl@gmail.com>
2025-08-18 15:09:14 -0300

    feat: add json editor page
    - add svelte-jsoneditor dependency
    - create json editor page with format, minify and validation tools
    - add route for json editor page
    - update home page to include link to json editor page
    - resolves #123

---
+++ src/routes/json/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import BackButton from "$lib/components/BackButton.svelte";
+    import { JSONEditor, Mode } from 'svelte-jsoneditor'
+    import { onMount } from 'svelte';
+
+    let mode: Mode = Mode.text; // ou Mode.text / Mode.tree
+    let editorRef: JSONEditor
+
+    onMount(() => {
+        document.documentElement.style.setProperty("--jse-font-size-mono", "1.2rem");
+    });
+
+</script>
+
+<svelte:head>
+    <title>JSON Editor</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</svelte:head>
+
+<BackButton />
+
+<h1>JSON Editor</h1>
+    
+<div class="main-card">
+    <div>
+      <JSONEditor bind:this={editorRef} {mode} indentation="    "  />
+    </div>
+</div>
+
+<style>
+.main-card {
+    max-width: 1000px;
+    min-height: 500px;
+}
+:global(.jse-main) {
+    height: 500px !important;
+}
+</style>
